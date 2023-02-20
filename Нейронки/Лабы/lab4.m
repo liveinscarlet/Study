9005d057ff5a57c3ffc6c2e27e9fb1d2aa6653b8
Anastasia Velichkina <95422871+liveinscarlet@users.noreply.github.com>
2023-02-20 17:27:16 +0300

    Лабы нейронки
    4, 5, 6  и 7 лабы

--- Нейронки/Лабы/lab4.m
+++ Нейронки/Лабы/lab4.m
@@ -1,20 +1,23 @@
 %% logial AND
-clear
+clear; clc
 P = [0 0 1 1; 0 1 0 1];
 T = [0 0 0 1];
-ANDNet = newp([0 1; 0 1], 1);
+ANDNet = [0 1; 0 1], 1;
 Y0 = sim(ANDNet,P);
 ANDNet.trainParam.epochs = 200;
 net = train(ANDNet,P,T);
 Y = sim(net,P)
 %% logical OR
-P = [0 0 1 1; 0 1 0 1];
+clear; clc
+P = [0 0 1 1, 0 1 0 1];
 T = [0 1 1 1];
-Y = net(P)
-net.trainParam.epochs = 20;
-net = train(net,P,T);
-Y = net(P)
+ORNet = [0 1; 1 1], 1;
+Y = sim(ORNet, P);
+net.trainParam.epochs = 200;
+net = train(ORNet,P,T);
+Y = sim(ORNet,P)
 %% logical XOR
+clear; clc
 P = [0 0 1 1; 0 1 0 1];
 T = [0 1 1 0];
 Y = net(P)
